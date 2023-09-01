f735d08727e43e302af6bd1da75ef623b2a10ffd
JuliaGolban <julia.j.shcherban@gmail.com>
2023-09-02 00:10:04 +0300

    fix get by filter

--- controllers/catalog/getByFilter.js
+++ controllers/catalog/getByFilter.js
@@ -72,8 +72,38 @@ const getByFilter = async (req, res, next) => {
 
       res.status(200).json({ catalog, total });
 
+      if (minPrice) {
+        filterConstructor.currentPrice = { currentPrice: { $gte: minPrice } };
+        total = await Catalog.find({
+          ...filterConstructor,
+        }).count();
+        constructorData.total = total;
+        catalog = await Catalog.find({
+          ...filterConstructor,
+        })
+          .limit(limit)
+          .skip(skip)
+          .sort({ createdAt: -1 });
+
+        res.status(200).json({ catalog, total });
+      }
+      if (maxPrice) {
+        (filterConstructor.currentPrice = { currentPrice: { $lte: maxPrice } }),
+          (total = await Catalog.find({
+            ...filterConstructor,
+          }).count());
+        constructorData.total = total;
+        catalog = await Catalog.find({
+          ...filterConstructor,
+        })
+          .limit(limit)
+          .skip(skip)
+          .sort({ createdAt: -1 });
+
+        res.status(200).json({ catalog, total });
+      }
       if (minPrice && maxPrice) {
-        filterConstructor.price = {
+        filterConstructor.currentPrice = {
           $and: [
             { currentPrice: { $gte: minPrice } },
             { currentPrice: { $lte: maxPrice } },
