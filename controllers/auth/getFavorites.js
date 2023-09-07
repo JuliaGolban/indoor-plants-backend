c18b67838f20d040dbc1471920edac0f9807ee19
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-07 16:30:37 +0300

    fix count of favorites

--- controllers/auth/getFavorites.js
+++ controllers/auth/getFavorites.js
@@ -2,26 +2,7 @@ const { Catalog } = require("../../models");
 const { Users } = require("../../models");
 
 const getFavorites = async (req, res, next) => {
-  //   const id = req.params.id;
-  //   console.log("req.params.id", req.params.id);
-  //   try {
-  //     const user = await Users.findById({ _id: id });
-  //     let data = [];
-
-  //     if (user.favorites !== "" && user.favorites !== undefined) {
-  //       for (let key of user.favorites) {
-  //          if (key !== "" && key !== undefined && key !== null) {
-  //           data.push(await Catalog.findOne({ article: +key }));
-  //         }
-  //       }
-  //     }
-  //     res.status(200).json(data);
-  //   } catch (error) {
-  //     throw new WrongIdError(error.message);
-  //   }
-  // };
-  const isPagination = req.query.page;
-  const { page, perPage } = req.query;
+  const { page = 1, perPage = 12 } = req.query;
   const id = req.params.id;
 
   try {
@@ -36,20 +17,15 @@ const getFavorites = async (req, res, next) => {
       }
     }
     let total = data.length;
-
-    if (isPagination) {
-      let catalog = [];
-      for (
-        i = page * perPage - perPage + 1;
-        i < page * perPage - perPage + 13 && i < data.length;
-        i++
-      ) {
-        catalog.push(data[i]);
-      }
-      res.status(200).json({ data: catalog, total });
-    } else {
-      res.status(200).json({ data, total });
+    let catalog = [];
+    for (
+      i = page * perPage - perPage + 1;
+      i < page * perPage + 1 && i < data.length;
+      i++
+    ) {
+      catalog.push(data[i]);
     }
+    res.status(200).json({ data: catalog, total });
   } catch (error) {
     res.status(400).json({ message: "Invalid characters" });
   }
