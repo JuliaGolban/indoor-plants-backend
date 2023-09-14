043c0b1b0e512ba55c6def730f20797c34500a28
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-09-14 14:44:32 +0300

    add ukrposhta departments

--- routes/api/cities.js
+++ routes/api/cities.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { cities } = require('../../controllers');
-const ctrlWrapper = require('../../middleWares/ctrlWrapper');
+const express = require("express");
+const { cities } = require("../../controllers");
+const ctrlWrapper = require("../../middleWares/ctrlWrapper");
 
-const { postByFilter } = cities;
+const { postByFilter, getByFilter } = cities;
 const router = express.Router();
 
-// router.get("/", ctrlWrapper(get));
-router.post('/', ctrlWrapper(postByFilter));
+router.post("/up", ctrlWrapper(getByFilter));
+router.post("/", ctrlWrapper(postByFilter));
 
 module.exports = routerSities = router;
