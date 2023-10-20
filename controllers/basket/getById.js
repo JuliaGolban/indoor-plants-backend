7c2c4f1a3d0fa03ccc086cfff1dbb981c34a9678
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-10-20 15:08:06 +0300

    getById

--- controllers/basket/getById.js
+++ controllers/basket/getById.js
@@ -4,9 +4,9 @@ const { Basket } = require("../../models");
 const getById = async (req, res, next) => {
   const _id = req.params.id;
   try {
-    let basketItems = []
+    let basketItems = {};
     if(_id !== ''){basketItems = await Basket.find({ _id })};
-    res.status(200).json({ data: basketItems });
+    res.status(200).json(basketItems);
   } catch (err) {
     throw new ValidationError(err.message);
   }
